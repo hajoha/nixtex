9624aa1e04d2520be1fadf8e4591fac5f556c732
Johann Hackler <johann.hackler@fokus.fraunhofer.de>
2025-04-27 13:31:15 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "LaTeX environment with SMILE and Awesome Beamer";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05"; # Or the latest stable branch
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -17,8 +17,8 @@
         src = pkgs.fetchFromGitHub {
           owner = "hajoha";
           repo = "smile";
-          rev = "master"; # Ideally pin to a real commit hash
-          sha256 = "sha256-eVHH92ZhVN5P9ynskIWrhHqKPWGt7nL1k9BXWboyiDY="; # Update if rev changes
+          rev = "861b33e29e323abf2e2eb0faf9b9057948d231e8";
+          sha256 = "sha256-eVHH92ZhVN5P9ynskIWrhHqKPWGt7nL1k9BXWboyiDY=";
         };
         installPhase = ''
           install -Dm644 smile.sty $out/tex/latex/smile/smile.sty
@@ -31,7 +31,7 @@
         src = pkgs.fetchFromGitHub {
           owner = "hajoha";
           repo = "awesome-beamer";
-          rev = "master"; # Ideally pin to a real commit hash
+          rev = "e1260b3f311e664c0d830481ce35c544e6aa28fd"; # Ideally pin to a real commit hash
           sha256 = "sha256-7n66MYKvIlP3bu/v7XAzLNEIn5/QUyd7LuqtLKT1aJY="; # Update if rev changes
         };
         installPhase = ''
@@ -39,37 +39,36 @@
         '';
       };
 
-      tex = pkgs.texlive.combine {
-        inherit (pkgs.texlive) scheme-basic;
-        packages = with pkgs.texlive; [
-          dvisvgm
-          dvipng
-          wrapfig
-          amsmath
-          ulem
-          hyperref
-          capt-of
-          latexmk
-          beamer
-          luaotfload-main
-        ];
+    tex = pkgs.texlive.combine {
+      inherit (pkgs.texlive) scheme-basic;
+      packages = with pkgs.texlive; {
+        dvisvgm = dvisvgm;
+        dvipng = dvipng;
+        wrapfig = wrapfig;
+        amsmath = amsmath;
+        ulem = ulem;
+        hyperref = hyperref;
+        capt-of = capt-of;
+        latexmk = latexmk;
+        beamer = beamer;
+        luaotfload = luaotfload;
       };
+    };
     in
-      {
+    rec {
         devShell = pkgs.mkShell {
           buildInputs = [
+            tex
             smile
             awesome-beamer
-            tex
-          ];
-
-          shellHook = ''
+         ];
+        };
+        shellHook = ''
             echo "Welcome to nixtex!"
             mkdir -p ./out
             touch main.tex
             alias compile='latexmk -pdf -pdflatex="pdflatex -interaction=nonstopmode -output-directory=./out" main.tex'
             alias clear='latexmk -CA && rm -rf ./out/* && rm -rf ./pkgs/'
           '';
-        };
       });
 }
